73d8f8032c3a3f86d9e63b146fa312dc553a37b1
mindxploit <bobbonicola@gmail.com>
2024-07-05 16:15:50 +0200

    if response is empty reset page at 1

--- reciper-app/app/page.tsx
+++ reciper-app/app/page.tsx
@@ -30,7 +30,7 @@ const Home = () => {
 
   const [searchText, setSearchText] = useState<string>("");
 
-  const [currentPage, setCurrentPage] = useState<number>(1)
+  const [currentPage, setCurrentPage] = useState<number>(1);
 
   useEffect(() => {
     //   // Fetch initial data for the dropdowns
@@ -52,6 +52,8 @@ const Home = () => {
     });
   }, []);
 
+  console.log(recipes.length);
+
   useEffect(() => {
     try {
       axios
@@ -66,96 +68,121 @@ const Home = () => {
           },
         })
         .then((response) => {
-        console.log(response.data, 'dd')
-          setRecipes(response.data);
+          response.data.length !== 0 ? setRecipes(response.data) : setCurrentPage(1);
         });
     } catch (e) {
       console.error(e);
     }
-  }, [searchText, cuisine, diet, difficulty, currentPage]);
+  }, [searchText, cuisine, diet, difficulty, currentPage, cuisines, diets, difficulties]);
+
+  useEffect(() => {
+    setCurrentPage(1);
+  }, [cuisine, diet, difficulty]);
 
   return (
-  <Box height={"100wh"}>
-    <Container maxW="container.md" >
-      <Box
-        as="header"
-        mb={5}
-        textAlign="center"
-        top="0"
-        bg="white"
-        zIndex={1}
-        py={4}
-      >
-        <Text fontSize="4xl" fontFamily="cursive">
-          Reciper
-        </Text>
-      </Box>
-      <Stack spacing={4} mb={3}>
-        <HStack spacing={2}>
-          <Input
-            onChange={(e) => setSearchText(e.target.value)}
-            placeholder="Search for recipes... 👨🏻‍🍳"
-          />
-          <IconButton aria-label="Search recipes" icon={<SearchIcon />} />
+    <Box height={"100wh"}>
+      <Container maxW="container.md">
+        <Box
+          as="header"
+          mb={5}
+          textAlign="center"
+          top="0"
+          bg="white"
+          zIndex={1}
+          py={4}
+        >
+          <Text fontSize="4xl" fontFamily="cursive">
+            Reciper
+          </Text>
+        </Box>
+        <Stack spacing={4} mb={3}>
+          <HStack spacing={2}>
+            <Input
+              onChange={(e) => setSearchText(e.target.value)}
+              placeholder="Search for recipes... 👨🏻‍🍳"
+            />
+            <IconButton aria-label="Search recipes" icon={<SearchIcon />} />
+          </HStack>
+        </Stack>
+        <HStack mb={5}>
+          <Select
+            placeholder="Cuisine"
+            value={cuisine}
+            onChange={(e) => setCuisine(e.target.value)}
+          >
+            {cuisines.length &&
+              cuisines.map((e) => (
+                <option key={e.id} value={e.name}>
+                  {e.name}
+                </option>
+              ))}
+          </Select>
+          <Select
+            placeholder="Diet"
+            value={diet}
+            onChange={(e) => setDiet(e.target.value)}
+          >
+            {diets.length &&
+              diets.map((e) => (
+                <option key={e.id} value={e.name}>
+                  {e.name}
+                </option>
+              ))}
+          </Select>
+          <Select
+            placeholder="Difficulty"
+            value={difficulty}
+            onChange={(e) => {
+              setDifficulty(e.target.value);
+            }}
+          >
+            {difficulties.length &&
+              difficulties.map((e) => (
+                <option key={e.id} value={e.name}>
+                  {e.name}
+                </option>
+              ))}
+          </Select>
         </HStack>
-      </Stack>
-      <HStack mb={5}>
-        <Select
-          placeholder="Cuisine"
-          value={cuisine}
-          onChange={(e) => setCuisine(e.target.value)}
-        >
-          {cuisines.length &&
-            cuisines.map((e) => (
-              <option key={e.id} value={e.name}>
-                {e.name}
-              </option>
-            ))}
-        </Select>
-        <Select
-          placeholder="Diet"
-          value={diet}
-          onChange={(e) => setDiet(e.target.value)}
-        >
-          {diets.length &&
-            diets.map((e) => (
-              <option key={e.id} value={e.name}>
-                {e.name}
-              </option>
-            ))}
-        </Select>
-        <Select
-          placeholder="Difficulty"
-          value={difficulty}
-          onChange={(e) => setDifficulty(e.target.value)}
-        >
-          {difficulties.length &&
-            difficulties.map((e) => (
-              <option key={e.id} value={e.name}>
-                {e.name}
-              </option>
-            ))}
-        </Select>
-      </HStack>
-      <Grid templateColumns="repeat(auto-fit, minmax(240px, 1fr))" gap={6}>
-        {recipes.map((recipe) => (
-          <CardRecipe
-            key={recipe.id}
-            {...recipe}
-            cuisine={cuisines[recipe.cuisineId - 1].name}
-            difficulty={difficulties[recipe.difficultyId - 1].name}
-            diet={diets[recipe.dietId - 1].name}
-          />
-        ))}
-      </Grid>
-    </Container>
+        <Grid templateColumns="repeat(auto-fit, minmax(240px, 1fr))" gap={6}>
+          {recipes.map((recipe) => (
+            <CardRecipe
+              key={recipe.id}
+              {...recipe}
+              cuisine={cuisines[recipe.cuisineId - 1]?.name}
+              difficulty={difficulties[recipe.difficultyId - 1]?.name}
+              diet={diets[recipe.dietId - 1]?.name}
+            />
+          ))}
+        </Grid>
+      </Container>
 
-      <HStack background={"white"} borderTop={"1px solid purple"} position={"absolute"} pt={6} bottom={10} right={0} left={0} justifyContent={"center"} spacing={3}>
-        <Button onClick={() => currentPage > 1 && setCurrentPage((prev) => prev - 1)}>{"<"}</Button>
+      <HStack
+        background={"white"}
+        borderTop={"1px solid purple"}
+        position={"absolute"}
+        pt={6}
+        bottom={10}
+        right={0}
+        left={0}
+        justifyContent={"center"}
+        spacing={3}
+      >
+        <Button
+          onClick={() => currentPage > 1 && setCurrentPage((prev) => prev - 1)}
+        >
+          {"<"}
+        </Button>
         <Text>Page: {currentPage}</Text>
-        <Button onClick={() => setCurrentPage((prev) => prev + 1)}>{">"}</Button>
+        <Button
+          onClick={() =>
+            recipes.length === 6 && setCurrentPage((prev) => prev + 1)
+          }
+        >
+          {">"}
+        </Button>
       </HStack>
-      </Box>
+    </Box>
   );
 };
 
